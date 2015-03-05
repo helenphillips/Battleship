03d1b80c053614565be4d8921e808a72440936b7
Helen Phillips <helen.r.phillips@googlemail.com>
2015-03-05 11:45:50 +0000

    bug fixed

--- battleship.rb
+++ battleship.rb
@@ -1,19 +1,19 @@
-module Battleship
+## module Battleship
 	class Board
 		SHIP_SIZES = {
-			aircraft_carrier: 5,
-			battleship: 4,
-			submarine: 3,
-			destroyer: 3,
-			patrol_boat: 2
+			:aircraft_carrier => 5,
+			:battleship => 4,
+			:submarine => 3,
+			:destroyer => 3,
+			:patrol_boat => 2
 		}
 
 		def initialize
 		## create grid
 			@coords = []
-			10.times do @coords.push([*0..9]) end
+			10.times{@coords.push([*0..9])}
 
-			SHIP_SIZES.each do |ship|
+			SHIP_SIZES.each_pair do |ship, size|
 				numbers =random_number
 				x = numbers[0]
 				y = numbers[1]
@@ -41,13 +41,22 @@ module Battleship
 			ok = true
 			while ok
 				x, y = random_number # destructuring the array
-				horizontal = [true, false].sample
+				horizontal = [true, false].choice
 				if horizontal == true
 				# if horizontal
-					block = [*x..x+(size-1)][y*3]
+					block_x = [*x..x+(size-1)]
+					block_y = []
+					size.times{block_y << y}
+					puts block_y
+					puts block_x
 				else# if vertical
-					block =[x*3][*y..y+(size-1)]
+					block_x =[]
+					size.times{block_x << x}
+					block_y = [*y..y+(size-1)]
+					puts block_y
+					puts block_x
 				end# test those coordinates against @coords
+				block = [block_x, block_y]
 				if !@coords[block].slice(size).include?('x')
 					@coords[block].each do |item|
 						item = 'x'
@@ -63,14 +72,18 @@ module Battleship
 				puts "Hit!"
 			else
 				puts "Miss!"
-
+			end
 		end
 		private
 
 		def random_number
 			return [rand(9), rand(9)]
 		end
-	end
-end
 
-Battleship.new()
+		def find_point
+			## function to take a multi-dimensional arrays to get a coordinate
+		end
+	end
+## end
+
+Board.new()
